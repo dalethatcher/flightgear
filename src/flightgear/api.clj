e5756f579afc9460f863542e080693b6de8d3f0c
Devin Walters <devinw@gmail.com>
2013-07-07 17:34:28 -0500

    Move docstrings to next line in api.clj

--- src/flightgear/api.clj
+++ src/flightgear/api.clj
@@ -1,57 +1,73 @@
 (ns flightgear.api
   (:require [flightgear.core :as core]))
 
-(defn connect "connect to the running flight gear using the telnet api" 
+(defn connect
+  "Connect to the running flight gear using the telnet api"
   [host port]
   (core/connect host port))
 
-(defn position "current position data as a map"
+(defn position
+  "Current position data as a map"
   []
   (core/position))
 
-(defn orientation "current orientation data as a map" 
+(defn orientation
+  "Current orientation data as a map"
   []
   (core/orientation))
 
-(defn velocities "current velocity data as a map" 
+(defn velocities
+  "Current velocity data as a map"
   []
   (core/velocities))
 
-(defn starter! "true/false to turn the starter key" 
+(defn starter!
+  "true/false to turn the starter key"
   [value]
   (core/starter! value))
 
-(defn aileron! "-1 to 1 to set aileron" 
+(defn aileron!
+  "-1 to 1 to set aileron"
   [value]
   (core/aileron! value))
 
-(defn elevator! "-1 to 1 to set elevator" 
+(defn elevator!
+  "-1 to 1 to set elevator"
   [value]
   (core/elevator! value))
 
-(defn rudder! "-1 to 1 to set rudder"
+(defn rudder!
+  "-1 to 1 to set rudder"
   [value]
   (core/rudder! value))
 
-(defn flaps! "0 to 1 to set flaps" 
+(defn flaps!
+  "0 to 1 to set flaps"
   [value]
   (core/flaps! value))
 
-(defn throttle! "0 to 1 to set throttle"
+(defn throttle!
+  "0 to 1 to set throttle"
   [value]
   (core/throttle! value))
 
-(defn indicated-airspeed-kt [] "indicated airspeed in knots"
+(defn indicated-airspeed-kt []
+  "indicated airspeed in knots"
   (core/indicated-airspeed-kt))
 
-(defn indicated-altitude-ft [] "indicated altitude in feet"
+(defn indicated-altitude-ft []
+  "indicated altitude in feet"
   (core/indicated-altitude-ft))
 
-(defn indicated-attitude [] "map of attitude in degrees, with keys :indicated-roll-deg and :indicated-pitch-deg"
+(defn indicated-attitude []
+  "map of attitude in degrees, with keys :indicated-roll-deg
+   and :indicated-pitch-deg"
   (core/indicated-attitude))
 
-(defn indicated-heading-deg [] "indicated heading in degrees from magnetic compass"
+(defn indicated-heading-deg []
+  "indicated heading in degrees from magnetic compass"
   (core/indicated-heading-deg))
 
-(defn engine-running? [] "true if the engine has started"
+(defn engine-running? []
+  "true if the engine has started"
   (core/engine-running?))
